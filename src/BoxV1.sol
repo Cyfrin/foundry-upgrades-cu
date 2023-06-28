f290dba95ca39940636e5f7e1b84eda220e51589
Bibash Tandon <bibashh958@gmail.com>
2023-06-28 20:57:30 +0530

    updated-imports

--- src/BoxV1.sol
+++ src/BoxV1.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts-upgradeable@4.8.3/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable@4.8.3/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable@4.8.3/proxy/utils/UUPSUpgradeable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 contract BoxV1 is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     uint256 internal value;
